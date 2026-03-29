3d2e4b51dfa652046f2684ad432a96bac7d2c2bc
andkstrr <andikasatrionurcahyo@gmail.com>
2026-03-29 12:48:45 +0700

    fix: server.js

--- src/server.js
+++ src/server.js
@@ -4,10 +4,15 @@ const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
 // Only listen when running locally (not on Vercel)
-if (process.env.NODE_ENV !== "production") {
-    app.listen(PORT, () => {
-        console.log(`server is running on port ${PORT}`);
-    });
-}
+// if (process.env.NODE_ENV !== "production") {
+//     app.listen(PORT, () => {
+//         console.log(`server is running on port ${PORT}`);
+//     });
+// }
+
+// production
+module.exports = (req, res) => {
+  app(req, res);
+};
 
 module.exports = app;
